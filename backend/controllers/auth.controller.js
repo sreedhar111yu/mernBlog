6bc72812b9875e5ef4ac248e9e991d232d24ffa3
sreedhar111yu <sreedharv36@gmail.com>
2024-02-13 21:56:42 +0530

    bcrypt hash pwd

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs'
 
 export const signup =async(req,res)=>{
     const{username, 
@@ -8,21 +9,23 @@ export const signup =async(req,res)=>{
            if(!username || !email || !password || username ==='' ||email ==='' || password===''){
             return res.status(400).json({message:'All fields are required'});
            }
-           const newuser =new User({
+
+           const hashedPassword = bcrypt.hashSync(password,10);
+           const newUser =new User({
             username,
             email,
-            password,
+            password:hashedPassword,
            });
 
-           try{
-            
-           await newuser.save();
-           res.json({message:'signin succes'})
-
-           }catch (error){
-            res.status().json({message:"error.message"})
-
-           }
-
+           try {
+            await newUser.save();
+            res.json({ message: 'Sign up successful' });
+        } catch (error) {
+            if (error.code === 11000 && error.keyPattern && error.keyPattern.hasOwnProperty('username')) {
+                res.status(400).json({ message: 'Username is already taken' });
+            } else {
+                res.status(500).json({ message: error.message });
+            }
+        }
 
 }
