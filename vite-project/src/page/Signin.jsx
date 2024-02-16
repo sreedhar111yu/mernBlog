56524f399ba7b79df14b7c7346be12f36e46dbc6
sreedhar111yu <sreedharv36@gmail.com>
2024-02-16 13:17:30 +0530

    signup in UI

--- vite-project/src/page/Signin.jsx
+++ vite-project/src/page/Signin.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import {Link,} from 'react-router-dom'
 
 function signin() {
+
+  
   return (
-    <div>signin</div>
+    <>
+    <div className='min-h-screen mt-20'>
+      <div className=''></div>
+       {/*left*/}
+      <div className=''></div>
+      {/*right*/}
+      <div className='p-3 max-w-lg mx-auto'>
+      <form  className='flex flex-col gap-4'>
+      
+      
+    
+    
+      <input type="email" placeholder='Email'
+      id='email' className='bg-slate-100 p-3 rounded-lg' 
+      />
+
+      <input type="password" placeholder='Password'
+      id='password' className='bg-slate-100 p-3 rounded-lg' 
+      />
+
+      <button className='bg-slate-700 text-white p-2 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        Sign In</button>
+        
+  
+
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Have an account? </p>
+      <Link to='/signup'> 
+      <span className='text-blue-500'>sign up</span>
+      </Link>
+      
+    </div>
+ 
+      </div>
+    </div>
+    
+    
+    </>
   )
 }
 
--- vite-project/src/page/Signup.jsx
+++ vite-project/src/page/Signup.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link,} from 'react-router-dom'
 
 function signup() {
+
+  const [formData, setFormData]=useState({});
+  const [error, setError]= useState(false);
+  const [loading,setLoading]= useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
   return (
     <>
     <div className='min-h-screen mt-20'>
@@ -8,15 +20,36 @@ function signup() {
        {/*left*/}
       <div className=''></div>
       {/*right*/}
-      <div className=''>
-        <form>
-          <div className=''>
-            <label value='your username'/>
-            <input type="text"placeholder='Username' 
-            id='username' />
-          </div>
-        </form>
+      <div className='p-3 max-w-lg mx-auto'>
+      <form  className='flex flex-col gap-4'>
+      
+      <input type="text" placeholder='Username'
+      id='username' className='bg-slate-100 p-3 rounded-lg' 
+      onChange={handleChange}/>
+    
+    
+      <input type="email" placeholder='Email'
+      id='email' className='bg-slate-100 p-3 rounded-lg' 
+      onChange={handleChange}/>
 
+      <input type="password" placeholder='Password'
+      id='password' className='bg-slate-100 p-3 rounded-lg' 
+      onChange={handleChange}/>
+
+      <button className='bg-slate-700 text-white p-2 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        Sign Up</button>
+        
+  
+
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Have an account? </p>
+      <Link to='/signin'> 
+      <span className='text-blue-500'>sign in</span>
+      </Link>
+      
+    </div>
+ 
       </div>
     </div>
     
